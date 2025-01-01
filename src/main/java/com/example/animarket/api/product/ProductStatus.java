22ce25647c19dea64e180231952e8f2200e10d22
yubincho <yubincho9@gmail.com>
2025-01-01 18:01:43 +0900

    first commit

---
+++ src/main/java/com/example/animarket/api/product/ProductStatus.java
@@ -0,0 +1,19 @@
+package com.example.animarket.api.product;
+
+
+import com.example.animarket.common.enum_interface.MyEnumInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@AllArgsConstructor
+@Getter
+public enum ProductStatus implements MyEnumInterface {
+
+    READY("준비중"),
+    AVAILABLE("판매중"),   // 판매 중
+    DISCONTINUED("판매 중단") // 판매 중단
+    ;
+
+    private final String value;
+}
---
+++ src/test/java/com/example/animarket/AnimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
